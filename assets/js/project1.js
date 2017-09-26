0ef9c633cced7146607676b11c0cf4b21b2e583d
David Thurman <dvdthurman@gmail.com>
2017-09-26 18:57:26 -0400

    New Files

---
+++ assets/js/project1.js
@@ -0,0 +1,13 @@
+// 1. Initialize Firebase - THIS IS SHELL CODE
+var config = {
+    apiKey: "AIzaSyBwwo-TebMOw-vankvkEo_pkVH8ULU191U",
+    authDomain: "classproject-9d2ad.firebaseapp.com",
+    databaseURL: "https://classproject-9d2ad.firebaseio.com",
+    projectId: "classproject-9d2ad",
+    storageBucket: "classproject-9d2ad.appspot.com",
+    messagingSenderId: "765615276448"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
