fc3507a629f1e9b8a108163e3dad56a3940fb929
David Thurman <dvdthurman@gmail.com>
2017-09-28 17:15:57 -0400

    AJAX Calls completed

--- assets/js/project1.js
+++ assets/js/project1.js
@@ -1,25 +1,128 @@
 $(document).ready(function() {
+   var football = ["Chicago Bears","Dallas Cowboys","Green Bay Packers","Los Angeles Rams","Miami Dolphins",
+                  "New England Patriots","New Orleans Saints","New York Giants","San Francisco 49ers",
+                  "Washington Redskins"];
+
+   var baseball = ["Baltimore Orioles","Boston Red Sox","Houston Astros","Kansas City Royals","Los Angeles Dodgers",
+                  "New York Yankees","Philadelphia Phillies","San Diego Padres","Seattle Mariners",
+                  "St Louis Cardinals"];
+
+  var basketball = ["Chicago Sky","Connecticut Sun","Indiana Fever","Los Angeles Sparks",
+                    "Minnesota Lynx","New York Liberty","Phoenix Mercury","Seattle Storm",
+                    "Washington Mystics"];
+
+  var br = "<br />";
+ 
+  for (var i = 0; i < football.length; i++) { 
+      var a = $("<a>"); 
+      $(a).addClass("dropdown-item");
+      $(a).attr("href","#");
+      $(a).text(football[i]);
+       $(a).text(football[i]);
+      $("#football").append(a); 
+      $("#football").append(br); 
+      
+  }
+
+  for (var i = 0; i < baseball.length; i++) {
+      var a = $("<a>");  
+      $(a).addClass("dropdown-item");
+      $(a).attr("href","#");
+      $(a).text(baseball[i]);
+      $("#baseball").append(a);
+      $("#baseball").append(br); 
+  }  
+
+  for (var i = 0; i < basketball.length; i++) {  
+      var a = $("<a>");
+      $(a).addClass("dropdown-item");
+      $(a).attr("href","#");
+      $(a).text(basketball[i]);
+      $("#basketball").append(a);
+      $("#basketball").append(br); 
+  }
+
+  // 1. Initialize Firebase
+
+  var config = {
+      apiKey: "AIzaSyC8JKgphcG7Jl21ZxUysApl3s6e38nTplc",
+      authDomain: "gameti-ddf37.firebaseapp.com",
+      databaseURL: "https://gameti-ddf37.firebaseio.com",
+      projectId: "gameti-ddf37",
+      storageBucket: "gameti-ddf37.appspot.com",
+      messagingSenderId: "394752220867"
+  };
+
+  if (!firebase.apps.length) {
+      firebase.initializeApp(config);
+  }
+
+  var database = firebase.database();               
+           
+
+// First AJAX call to mysportsfeed api to return game data
+
+// some arugment here to capture inout team
+// get date range from input form here
+
+  $.ajax
+({
+  type: "GET",
+  url: "https://api.mysportsfeeds.com/v1.1/pull/nfl/2017-regular/full_game_schedule.json?date=20171001",
+  dataType: 'json',
+  headers: {
+ "Authorization": "Basic " + btoa('doctort57' + ':' + 'Rowanp1996' )
+
+
+},
+  
+  success: function (data){
+      //    console.log(JSON.stringify(data));
+     // get response data into var
+          var results = (JSON.stringify(data));
+ 
+         for (var i = 0; i < data.fullgameschedule.gameentry.length; i++) {
+
+                var city = data.fullgameschedule.gameentry[i].id;
+                console.log(city);
+                city = data.fullgameschedule.gameentry[i].homeTeam.City;
+                console.log(city);
+
+
+           }
+
+  }
+});
 
 
 
-	$("#player-form").hide();
+  $.ajax
+({
+  type: "GET",
+  url: "https://maps.googleapis.com/maps/api/place/textsearch/json?query=Restaurants+near+Giants+Stadium&key=AIzaSyC8JKgphcG7Jl21ZxUysApl3s6e38nTplc",
+  dataType: 'json',
+  
 
-// 1. Initialize Firebase
+  success: function (data){
+    //    console.log(JSON.stringify(data));
+     // get response data into var
+         
+ 
+           
 
-	var config = {
-	    apiKey: "AIzaSyC8JKgphcG7Jl21ZxUysApl3s6e38nTplc",
-	    authDomain: "gameti-ddf37.firebaseapp.com",
-	    databaseURL: "https://gameti-ddf37.firebaseio.com",
-	    projectId: "gameti-ddf37",
-	    storageBucket: "gameti-ddf37.appspot.com",
-	    messagingSenderId: "394752220867"
-	};
+               
+                var city = data.results[0].formatted_address;
+                console.log(city);
 
-	if (!firebase.apps.length) {
-	    firebase.initializeApp(config);
-	}
 
-	var database = firebase.database();
+  }
+});
+
+ 
+ 
+ 
+
+  
 
 // At the initial load and on subsequent data value changes, get a snapshot of the current data. (I.E FIREBASE HERE)
 // This callback keeps the page updated when a value changes in firebase.
@@ -121,7 +224,6 @@ $(document).ready(function() {
 
 	$(document).on("click", ".somefield", somefunction);
   	$(document).on("click", ".somefield", somefunction); */
-
 });
 
 
